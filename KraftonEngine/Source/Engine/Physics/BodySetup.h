7fcd82937a131712e3484db338483d4cc2b8e654
Seyoung <seyoungpark@hanyang.ac.kr>
2026-05-30 16:15:46 +0900

    add: BodySetup

---
+++ KraftonEngine/Source/Engine/Physics/BodySetup.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "Physics/BodySetupCore.h"
+#include "Physics/PhysicsGeometry.h"
+
+#include "Source/Engine/Physics/BodySetup.generated.h"
+
+UCLASS()
+class UBodySetup : public UBodySetupCore
+{
+public:
+    GENERATED_BODY()
+
+    UBodySetup() = default;
+    ~UBodySetup() override = default;
+
+    FKAggregateGeom& GetAggGeom() { return AggGeom; }
+    const FKAggregateGeom& GetAggGeom() const { return AggGeom; }
+
+    bool HasGeometry() const;
+
+public:
+    FKAggregateGeom AggGeom;
+};
